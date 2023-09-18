22d6572867dbdaa28cee58e46f426162a0c05fc4
Ain1204 <emily030528@gmail.com>
2023-09-18 17:30:51 +0900

    feat/백업

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import firebase from './firebase';
-console.log(firebase);
+import {authService} from 'fbase';
+console.log(authService);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
