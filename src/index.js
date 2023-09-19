1e7931ac1e34e2e634710e8430f155a4155bfb5b
Ain1204 <emily030528@gmail.com>
2023-09-19 18:59:25 +0900

    feat/로그인연결

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-import {authService} from 'fbase';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import { authService } from "fbase";
 console.log(authService);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
